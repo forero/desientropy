6f8eaec28deffb3d1200d1d32f4618abf30a1301
Jaime E Forero-Romero <j.e.forero.romero@gmail.com>
2022-03-08 11:40:56 -0800

    add 2d entropy

--- py/desientropy/compute.py
+++ py/desientropy/compute.py
@@ -16,3 +16,21 @@ def entropy_1d(x):
     h = np.sum(-p*np.log2(p))/np.log2(24)
     return h
 
+def entropy_2d(x):
+    n_steps_x = np.shape(x)[0] - 3
+    n_steps_y = np.shape(x)[1] - 3
+    proba = {}
+    for i in range(n_steps_x):
+        for j in range(n_steps_y):
+            d  = x[i:i+2, j:j+2].flatten()
+            l = list(np.argsort(d))
+            l = ''.join(str(e) for e in l)
+            try:
+                proba[l] += 1
+            except:
+                proba[l] = 1
+            p = np.array(list(proba.values()))
+            p = p/p.sum()
+            h = np.sum(-p*np.log2(p))/np.log2(24)
+    return h
+
